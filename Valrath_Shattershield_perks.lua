afe47bc947066cdc3cabbbcdf4947e0c3cf1af2b
SteelDaDiamond <steelsmyname@gmail.com>
2024-10-21 23:32:02 -0700

    Add files via upload

---
+++ Valrath_Shattershield_perks.lua
@@ -0,0 +1,72 @@
+-- Perk List
+perks = {
+  [1]={
+    remove={"(-1)"},
+    add={"(+0) (shield) (icon)"},
+  },
+  [2]={
+    remove={"(-1)"},
+    add={"(+0) (shield) (icon)"},
+  },
+  [3]={
+    remove={"(-1)"},
+    add={"(+0) (shield) (icon)"},
+  },
+  [4]={
+    remove={"(-2)"},
+    add={"(-2) (fa:flask-round-potion) (Add +1 (attack) for each Shield you have this round)"},
+  },
+  [5]={
+    remove={"(-1)","(-1)"},
+  },
+  [6]={
+    remove={"(+0)"},
+    add={"(+1) curse"},
+  },
+  [7]={
+    remove={"(+0)"},
+    add={"(+1) curse"},
+  },
+  [8]={
+    remove={"(+0)"},
+    add={"(+1) curse"},
+  },
+  [9]={
+    remove={"(+0)"},
+    add={"rolling ward"},
+  },
+  [10]={
+    remove={"(+0)"},
+    add={"rolling ward"},
+  },
+  [11]={
+    remove={"(+1)"},
+    add={"(+3) fire"},
+  },
+  [12]={
+    remove={"(+1)"},
+    add={"rolling (+1) regenerate"},
+  },
+  [13]={
+    remove={"(+1)"},
+    add={"rolling (+1) regenerate"},
+  },
+  [14]={
+    remove={"(+1)"},
+    add={"(+2) wound"},
+  },
+  [15]={
+    remove={"(+1)"},
+    add={"(+2) wound"},
+  },
+  [16]={
+    ignore=ClassApi.PerkType.IgnoreItem,
+    --
+    add={"Perk Reminder #16"},
+  },
+  [17]={},
+  [18]={
+    --Once each scenario, you may  of negative conditions for the round
+    add={"Perk Reminder #18"},
+  },
+},
